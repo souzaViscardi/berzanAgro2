d6d6604dbff77772631429c184131211df35aca3
Kevin  viscardi de souza <viscardi.souza@gmail.com>
2024-11-02 16:07:17 -0300

    fix leaft

--- src/Components/Mapa/Mapa.tsx
+++ src/Components/Mapa/Mapa.tsx
@@ -1,22 +1,36 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap  } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useEffect } from 'react';
+
+interface MyMapProps {
+    center: [number, number];
+    zoom: number;
+  }
 
 
-
-const Mapa: React.FC = () => {
-    const position: [number, number] =  [-23.615370, -47.045270];
-  
+const Mapa = () => {
+    const center: [number, number] =  [-23.615370, -47.045270];
+    const zoom: number =  10;
+    const SetViewOnLoad = ({ center, zoom }: MyMapProps) => {
+      const map = useMap();
+    
+      useEffect(() => {
+        map.setView(center, zoom);
+      }, [map, center, zoom]);
+    
+      return null;
+    };
     return (
-      <MapContainer center={position} zoom={10} style={{ height: '400px', width: '100%' }}>
+      <MapContainer  style={{ height: '400px', width: '100%' }}>
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={position}>
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        <Marker position={center}>
           <Popup>
             Localização aqui!
           </Popup>
         </Marker>
+        <SetViewOnLoad center={center} zoom={zoom} />
+
       </MapContainer>
     );
   };
