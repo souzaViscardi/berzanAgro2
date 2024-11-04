1f9accb01e087578f8f9e9ce658e8f0319c72fcb
Kevin  viscardi de souza <viscardi.souza@gmail.com>
2024-11-04 15:37:25 -0300

    fix products

--- src/Pages/Works/Works.tsx
+++ src/Pages/Works/Works.tsx
@@ -12,29 +12,40 @@ export default function Works (){
 
     const paramValue = searchParams.get('idProduct');
     const [isFilter, setIsFilter] = useState(paramValue || false);
-
+    const [title, setTitle] = useState("Pecuária");
+    const [img, setImg] = useState("/Foto_Pecuaria.jpg");
     useEffect(() => handleFilter(paramValue), [paramValue]);
     function handleFilter(filter:string|null){
         setIsFilter(!isFilter);
         if(filter === "pecuaria"){
             setSelected(pecuaria);
+            setTitle("Pecuária");
+            setImg("/Foto_Pecuaria.jpg");
         }else if(filter === "industrial"){
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             setSelected(Industrial as any);
+            setTitle("Industriais");
+            setImg("/Foto_Industria.jpg");
+
         }else if(filter === "fertilizantes"){
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             setSelected(fertilizantes as any);
+            setTitle("Fertilizantes");
+            setImg("/Foto_Fertilizantes.jpg");
         }
     }
     return(
         <>
         <NavBar></NavBar>
+        <div className="videoContainerProducts">
+                <div className="titleOverflowProducts">
+                    <h1>Nossos Produtos</h1>
+                    <h1>{title}</h1>
+                </div>
+                 <img  className="aboutImgProducts" src={img} />            
+                </div>
         <div id="Produtos" className="full-container">
             <ScrollRestoration />
-            <br></br>
-            <h1>    
-                Nossos Produtos
-            </h1>
             <ul id="filter">
                 <li><a onClick={() => handleFilter("pecuaria")}>Pecuária</a></li>
                 <li><a onClick={() => handleFilter("industrial")}>Industrial</a></li>
