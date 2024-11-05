97cfd0330143f5244c3809b27017ba960fff044e
Kevin  viscardi de souza <viscardi.souza@gmail.com>
2024-11-05 11:27:09 -0300

    add links to footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -21,21 +21,21 @@ export default function Footer() {
                           <img src="/wpp.png" alt="logo"/>
                             <div className="info-box">
                               <h2>WhatsApp</h2>
-                              <a>+55 (11) 94299-2691</a>
+                              <a target="blank" href="https://api.whatsapp.com/send?phone=5511942992691&text=Olá%20Gostaria%20de%20saber%20mais%20sobre%20os%produtos%20da%berzan.">+55 (11) 94299-2691</a>
                             </div>
                       </Container>
                       <Container>
                           <img src="/email.png" alt="logo"/>
                           <div className="info-box">
                               <h2>Email</h2>
-                              <a>lucaszanni.berzan@outlook.com</a>
+                              <a href="mailto:lucaszanni.berzan@outlook.com">lucaszanni.berzan@outlook.com</a>
                             </div>
                       </Container>
                       <Container>
                           <img src="/linkedin.png" alt="logo"/>
                           <div className="info-box">
                               <h2>Linkedin</h2>
-                              <a>Berzan Agro</a>
+                              <a href="">Berzan Agro</a>
                             </div>
                       </Container>
                     </div>
