1cfe33732fdaf6dddcd8fa2cbecb68332b573baf
Kevin  viscardi de souza <viscardi.souza@gmail.com>
2024-11-11 08:57:27 -0300

    add info addres

--- src/Components/Mapa/Mapa.tsx
+++ src/Components/Mapa/Mapa.tsx
@@ -1,4 +1,5 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap  } from 'react-leaflet';
+import {useState} from "react";
 import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 import L from "leaflet";
@@ -16,13 +17,26 @@ interface MyMapProps {
 
 
 const Mapa = () => {
+    const [address, setAddress] = useState("")
     const center: [number, number] =  [-23.615370, -47.045270];
     const zoom: number =  10;
+    const getAddressFromCoords = async (lat: number, lon: number) => {
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&addressdetails=1`
+      );
+      const data = await response.json();
+      console.log(data);
+      if (data && data.address) {
+        const formattedAddress = `${data.address.road}, ${data.address.city_district}, ${data.address.country}`;
+        setAddress(formattedAddress);
+      }
+    };
     const SetViewOnLoad = ({ center, zoom }: MyMapProps) => {
       const map = useMap();
     
       useEffect(() => {
         map.setView(center, zoom);
+        getAddressFromCoords(center[0], center[1]);
       }, [map, center, zoom]);
     
       return null;
@@ -33,7 +47,14 @@ const Mapa = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
         <Marker position={center} icon={customIcon}>
           <Popup>
-            Localização aqui!
+            <div>
+              <strong>Coordenadas:</strong><br />
+              Latitude: {center[0]} <br />
+              Longitude: {center[1]} <br /><br />
+              
+              <strong>Endereço:</strong><br />
+              {address ? address : 'Carregando endereço...'}
+            </div>          
           </Popup>
         </Marker>
         <SetViewOnLoad center={center} zoom={zoom} />
